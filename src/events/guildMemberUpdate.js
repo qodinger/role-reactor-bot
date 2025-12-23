f27b103e4538d987c34044839d35bfe6f00b2d0e
tyecode <sengphachanh.dev@gmail.com>
2025-12-23 14:58:18 +0700

    fix(lint): fix unused variable errors in guildMemberUpdate
    - Remove unused getLogger import
    - Prefix unused parameters with underscore to indicate intentional non-use

--- src/events/guildMemberUpdate.js
+++ src/events/guildMemberUpdate.js
@@ -1,13 +1,12 @@
 import { Events } from "discord.js";
-import { getLogger } from "../utils/logger.js";
 
 /**
  * Handle guild member updates (including role changes)
- * @param {import("discord.js").GuildMember} oldMember - Previous member state
- * @param {import("discord.js").GuildMember} newMember - New member state
+ * @param {import("discord.js").GuildMember} _oldMember - Previous member state
+ * @param {import("discord.js").GuildMember} _newMember - New member state
  * @param {import("discord.js").Client} _client - Discord client (passed by event handler, unused)
  */
-export async function execute(oldMember, newMember, _client) {
+export async function execute(_oldMember, _newMember, _client) {
   // Role changes are handled by Discord's permission system
 }
 
