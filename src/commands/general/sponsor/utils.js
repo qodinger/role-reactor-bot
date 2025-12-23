27ad066ed24889c3aa1f1a73b5faf385bdd6c5ac
tyecode <sengphachanh.dev@gmail.com>
2025-12-23 11:04:49 +0700

    chore: remove unused files

--- src/commands/general/sponsor/utils.js
+++
@@ -1,7 +0,0 @@
-/**
- * Utility functions for the sponsor command
- */
-
-export function placeholder() {
-  return "This function is a placeholder for future utilities.";
-}
--- src/commands/general/support/utils.js
+++
@@ -1,7 +0,0 @@
-/**
- * Utility functions for the support command
- */
-
-export function placeholder() {
-  return "This function is a placeholder for future utilities.";
-}
