6a29243cc136b179476f00afb21571860110753a
tyecode <sengphachanh.dev@gmail.com>
2025-12-27 16:30:21 +0700

    refactor(commands): remove sponsor command

--- src/commands/general/sponsor/index.js
+++
@@ -1,8 +0,0 @@
-import { SlashCommandBuilder } from "discord.js";
-import { execute } from "./handlers.js";
-
-export const data = new SlashCommandBuilder()
-  .setName("sponsor")
-  .setDescription(`Get information about supporting the bot's development`);
-
-export { execute };
